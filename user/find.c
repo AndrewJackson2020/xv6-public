b97b7aa4a16ec0cb486d0cf6994d3d04b3009dfb
CommanderKeynes <andrewjackson947@gmail.coma>
2026-02-24 18:53:39 -0600

    Remove unused function

--- user/find.c
+++ user/find.c
@@ -5,26 +5,6 @@
 #include "kernel/fs.h"
 #include "kernel/fcntl.h"
 
-char*
-fmtname(char *path)
-{
-  static char buf[DIRSIZ+1];
-  char *p;
-
-  // Find first character after last slash.
-  for(p=path+strlen(path); p >= path && *p != '/'; p--)
-    ;
-  p++;
-
-  // Return blank-padded name.
-  if(strlen(p) >= DIRSIZ)
-    return p;
-  memmove(buf, p, strlen(p));
-  memset(buf+strlen(p), ' ', DIRSIZ-strlen(p));
-  buf[sizeof(buf)-1] = '\0';
-  return buf;
-}
-
 int find(char *path){
   char buf[512], *p;
   int fd;
